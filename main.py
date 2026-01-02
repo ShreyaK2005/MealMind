53246bdc37f44d71af8b62711f10ab777673050b
Shreya Kasture <kasturesa@rknec.edu>
2026-01-01 22:15:10 -0700

    Delete main.py

--- main.py
+++ main.py
@@ -1,23 +1,3 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-from meal_engine import generate_meal_plan
 
-app = FastAPI()
 
-class UserInput(BaseModel):
-    age: int
-    gender: str
-    height: float
-    weight: float
-    goal: str
-    diet_type: str
-    health_conditions: list
-    allergies: list
-    country: str
-
-@app.post("/generate_meal_plan")
-def get_meal_plan(user: UserInput):
-    user_data = user.dict()
-    meal_plan = generate_meal_plan(user_data)
-    return {"meal_plan": meal_plan}
 
